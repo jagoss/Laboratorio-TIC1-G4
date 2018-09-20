145778f1d2f441fe34e3bbc1a83e146b0c0946ae
JuanJoseR <jregent@correo.um.edu.uy>
2018-09-20 17:18:00 -0300

    UI busqueda restaurante v1.0

---
+++ TICC/src/main/java/uy/edu/um/UI/controllers/PrimeraPantallaController.java
@@ -0,0 +1,63 @@
+package uy.edu.um.UI.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.AnchorPane;
+
+public class PrimeraPantallaController {
+
+     @FXML
+     private CheckBox checkPocitos;
+
+     @FXML
+     private CheckBox checkPuntaCarretas;
+
+     @FXML
+     private CheckBox checkBuceo;
+
+     @FXML
+     private CheckBox check5Estrellas;
+
+     @FXML
+     private CheckBox check4Estrellas;
+
+     @FXML
+     private CheckBox check3Estrellas;
+
+     @FXML
+     private CheckBox checkTipoAsado;
+
+     @FXML
+     private CheckBox checkTipoSushi;
+
+     @FXML
+     private CheckBox checkTipoVegana;
+
+     @FXML
+     private AnchorPane opcionRestaurante1;
+
+     @FXML
+     private AnchorPane opcionRestaurante2;
+
+     @FXML
+     private AnchorPane opcionRestaurante3;
+
+     @FXML
+     private AnchorPane opcionRestaurante4;
+
+     @FXML
+     private AnchorPane opcionRestaurante5;
+
+     @FXML
+     private AnchorPane opcionRestaurante6;
+
+     @FXML
+     void chequearPocitos(ActionEvent event) {
+
+
+
+     }
+ }
+
+
