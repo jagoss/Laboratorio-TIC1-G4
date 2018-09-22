38ee1e16c488bbb0bb51031c9117ec7e1c595276
juangossweiler <juan.agossweiler@gmail.com>
2018-09-22 17:53:26 -0300

    Test Springboot

--- TICC/src/main/java/um/edu/uy/bbticg4/service/entities/RestaurantMgr.java
+++ TICC/src/main/java/um/edu/uy/bbticg4/service/entities/RestaurantMgr.java
@@ -1,25 +1,83 @@
 package um.edu.uy.bbticg4.service.entities;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import um.edu.uy.bbticg4.exceptions.InvalidRestoInformation;
 import um.edu.uy.bbticg4.exceptions.RestoAlreadyExists;
 import um.edu.uy.bbticg4.persistence.RestaurantRepository;
+
+import java.util.Optional;
 
 @Service
 public class RestaurantMgr {
 
-    @Autowired
-    private RestaurantRepository restoRepo;
+    private RestaurantRepository restoRepo = new RestaurantRepository() {
+        @Override
+        public <S extends Restaurant> S save(S entity) {
+            return null;
+        }
 
-    public void addRestaurant(Integer id, String name, String barrio) throws RestoAlreadyExists {
+        @Override
+        public <S extends Restaurant> Iterable<S> saveAll(Iterable<S> entities) {
+            return null;
+        }
 
-        if(restoRepo.findById(id) != null){
+        @Override
+        public Optional<Restaurant> findById(Integer integer) {
+            return Optional.empty();
+        }
+
+        @Override
+        public boolean existsById(Integer integer) {
+            return false;
+        }
+
+        @Override
+        public Iterable<Restaurant> findAll() {
+            return null;
+        }
+
+        @Override
+        public Iterable<Restaurant> findAllById(Iterable<Integer> integers) {
+            return null;
+        }
+
+        @Override
+        public long count() {
+            return 0;
+        }
+
+        @Override
+        public void deleteById(Integer integer) {
+
+        }
+
+        @Override
+        public void delete(Restaurant entity) {
+
+        }
+
+        @Override
+        public void deleteAll(Iterable<? extends Restaurant> entities) {
+
+        }
+
+        @Override
+        public void deleteAll() {
+
+        }
+    };
+
+    public void addRestaurant(Integer id, String name, String barrio) throws RestoAlreadyExists, InvalidRestoInformation {
+
+        if(id == null || name == null || barrio == null || "".equals(name)|| "".equals(id) || "".equals(barrio)){
+            throw new InvalidRestoInformation();
+        }
+
+        if(restoRepo.existsById(id)){
             throw new RestoAlreadyExists();
         }
 
-        Restaurant oResto = new Restaurant(id, name, barrio);
-
-        restoRepo.save(oResto);
+        restoRepo.save(new Restaurant(id, name, barrio));
     }
 
 }
