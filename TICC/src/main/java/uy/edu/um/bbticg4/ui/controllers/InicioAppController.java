d1dadf2dd58cf23901cdeda0cacfbbb110e64624
aterra1 <aterra1@correo.um.edu.uy>
2018-10-15 15:48:02 -0300

    agregados bases de inicio y log in

---
+++ TICC/src/main/java/uy/edu/um/bbticg4/ui/controllers/InicioAppController.java
@@ -0,0 +1,37 @@
+package uy.edu.um.bbticg4.ui.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class InicioAppController {
+
+    @FXML
+    private Button signInButton;
+
+    @FXML
+    private Button SignUpButton;
+
+    @FXML
+    void SignUp(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signIn(ActionEvent event) {
+
+        FXMLLoader fxmlLoader = new FXMLLoader();
+       // fxmlLoader.setControllerFactory(NUEVO MAIN?????.getContext()::getBean); NUEVO MAIN????
+
+        Parent root = fxmlLoader.load(LogInController.class.getResourceAsStream("LogIn.fxml"));
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.show();
+
+    }
+
+}
---
+++ TICC/src/main/java/uy/edu/um/bbticg4/ui/controllers/LogInController.java
@@ -0,0 +1,38 @@
+package uy.edu.um.bbticg4.ui.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LogInController {
+
+    @FXML
+    private Button InicioButton;
+
+    @FXML
+    private Button goBackButton;
+
+    @FXML
+    private TextField userMail;
+
+    @FXML
+    private PasswordField userPass;
+
+    @FXML
+    private ChoiceBox<?> opcionLog;
+
+    @FXML
+    void confirmation(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goBack(ActionEvent event) {
+
+    }
+
+}
+
